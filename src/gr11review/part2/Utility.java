d15ef192fbc2b9d2b446312ff8f98a659e096b5d
matthewm11 <matthew.morelli21@ycdsbk12.ca>
2020-11-25 22:54:33 +0000

    started the sumNumbers function

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,40 @@
 package gr11review.part2;
+import java.lang.*;
 
 public class Utility {
     
+  public static int sumNumbers(String str) {
+
+    boolean isChar;
+    int numOfConsecutiveChars = 0;
+    int length = str.length();
+    int count;
+    String currentNum;
+    int total = 0;
+    String currentSubstring;
+
+    for (count = 0; count < length; count++) {
+
+      currentSubstring = str.substring(count, count + 1);
+
+      isChar = Character.isDigit(currentSubstring);
+
+      if (isChar) {
+
+        numOfConsecutiveChars++;
+
+        currentNum += currentSubstring;
+        
+      } else {
+
+        total += Integer.parseInt(currentNum);
+
+        numOfConsecutiveChars = 0;
+
+      }
+
+    }
+
+  }
+
 }
