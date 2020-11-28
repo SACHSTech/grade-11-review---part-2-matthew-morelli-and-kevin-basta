f858f1d63858a0d63fe70ee67a3dd331ad883e80
addisonc1 <addison.chan21@ycdsbk12.ca>
2020-11-28 23:58:56 +0000

    progress

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -5,9 +5,15 @@ import gr11review.part2.Utility.*;
 public class Main{
   public static void main(String[] args) throws IOException{
   String strsentence;
+  String strnewsentence;
+  String word;
   BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
   System.out.println("Enter the sentence");
-  strsentence = keyboard.readLine();   
+  strsentence = keyboard.readLine(); 
+  System.out.println("Enter the only character to be shown");
+  word = keyboard.readLine(); 
+  System.out.println (plusOut(strsentence));
+
 
   }
 
--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,11 +1,45 @@
 package gr11review.part2;
-
+import java.io.world.txt;
+import java.io.*;
 public class Utility {
-    public static String plusOut (String str, String word) {
+  public static String plusOut (String str, String word) {
+      String strsentence;
+      int intcount;
+      String strword;
+      strword = "";
+      strsentence = str;
+      for(intcount = 0; intcount < str.length(); intcount++){
+        if(str){
+        
+        
+        }else{
+        
+        
+        }
+     }
 
 
 
 
     }
 
+  public static double avgWord(String world){
+      int readingword;
+      int finalnumber;
+      int average;
+      File words = new File("world.txt");
+      Scanner reading = new Scanner(words);
+      int intcount2;
+      while(words.hasNextLine()){
+        readingworld = words.nextLine();
+        intcount2 = intcount2 +1;
+        finalnumber = readingworld + finalnumber;
+
+
+
+        }
+        average = finalnumber/intcount2;
+      }
+
+  }
 }
