61889f3136f4eabc6f9a4354ff43091691ef9f65
matthewm11 <matthew.morelli21@ycdsbk12.ca>
2020-12-01 00:42:45 +0000

    deleted the text files

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -5,28 +5,6 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws IOException {
-    /*
-    String sentence;
-
-    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-
-    System.out.println("Enter a sentence: ");
-    sentence = keyboard.readLine();
-
-    System.out.println(Utility.sumNumbers(sentence));
-
-    System.out.println(Utility.alphaWord("src/gr11review/part2/" + "words.txt"));
-
-    int[] nums = {1, 2, 3, 2, 5, 2};
-
-    System.out.println(Arrays.toString(Utility.notAlone(nums, 2)));
-
-    int[] nums2 = {1, 1, 1, 2, 1};
-
-    System.out.println(Utility.canBalance(nums2));
-
-    Utility.diagonal(8);
-    */
 
   }
 }
