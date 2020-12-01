8763b2b11415abf8460f5645fe365935e993f23c
KevinBasta <Kevin.basta22@ycdsbK12.ca>
2020-12-01 00:41:06 +0000

    getting rid of test code and deleting txt files

--- src/gr11review/part2/Main.java
+++ src/gr11review/part2/Main.java
@@ -7,30 +7,6 @@ import java.io.*;
 
 public class Main{
   public static void main(String[] args) throws IOException{
-    /*
-    BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-    String strUserInput;
-    String strOutPut;
-    strUserInput = keyboard.readLine();
-
-    strOutPut = Utility.zipZap(strUserInput);
-    System.out.println(strOutPut);
-    */
-
-    //String strFile = "RandomWords.txt";
-    //System.out.println(Utility.longestWord(strFile));
-
-    /*
-    int[] testArray = {10, 1, 9, 20};
-    System.out.println(Arrays.toString(Utility.tenRun(testArray)));
-    */
-
-    /*
-    int[] outer = {1, 2, 4, 6}; 
-    int[] inner = {2, 3, 4};
-    System.out.println(Utility.linearIn(outer, inner));
-    */
-
-    //Utility.pascalTri(3, 4);
+    
   }
 } 
